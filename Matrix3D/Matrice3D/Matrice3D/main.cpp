5a9c4e6bb3dce766a3f2d72a04d98bff9e55307a
Simone-Tombolini <simone.tombolini@gmail.com>
2022-12-30 02:48:42 +0100

    some operator/function
    size()
    slice()
    constructor(z,y,x)

--- Matrix3D/Matrice3D/Matrice3D/main.cpp
+++ Matrix3D/Matrice3D/Matrice3D/main.cpp
@@ -4,37 +4,91 @@
 #include "matrix3D.h"
 
 using namespace std;
-void test_metodi_fonamentali() 
+void test_metodi_fonamentali_primitivi(bool set = true) 
 {
-    #ifndef NDEBUG
-        cout << "---------------------Test metodi fondamentali---------------------" << endl;
-    #endif
-   
-    //creazione di una matrice vuota, test costruttore di default
-    matrix3D<int> matrice0; 
-    
-    //creazione di una matrice con il valore di default 0.1, test costruttore secondario
-    cout << "Matrice con metodo costruttore secondario" << endl;
-    matrix3D<double> matrice1(4, 2, 3, 0.1);
+    if (set == true) {
+        cout << "---------------------Test metodi fondamentali primitivi---------------------" << endl;
 
-    cout << "Matrice con metodo costruttore per copia" << endl;
-    matrix3D<double> matrice2(matrice1);
 
-    cout << "valore non modificato " << matrice1(1, 1, 1) << endl;
-    matrice1(1, 1, 1) = 5;
-    cout << "valore modificato " << matrice1(1, 1, 1) << endl;
-    
-    matrice2 = matrice1;
-    //prova di stampa matrice
-    cout << matrice1;
+        //creazione di una matrice vuota, test costruttore di default
+        matrix3D<int> matrice0;
+
+        //creazione di una matrice con il valore di default 0.1, test costruttore secondario
+        cout << "Matrice con costruttore secondario con default value" << endl;
+        matrix3D<double> matrice1(4, 2, 3, 0.1);
+
+        cout << endl;
+        cout << "Matrice con costruttore secondario senza default value" << endl;
+        matrix3D<float> matrice3(3, 4, 3);
+
+        cout << endl;
+        cout << "Matrice con metodo costruttore per copia" << endl;
+        matrix3D<double> matrice2(matrice1);
+
+        cout << endl;
+
+        cout << "operatore assegnamento" << endl;
+        matrice2 = matrice1;
+
+
+
+        //prova di stampa matrice
+
+      
+    }
 }
+void test_metodi_ausiliari_primitivi(bool set = true) {
+    if (set == true) {
+        cout << "---------------------Test metodi ausiliari primitivi---------------------" << endl;
+        cout << "creazione matrice di test" << endl;
+        matrix3D<double> matrice1(4, 2, 3, 0.2);
+        cout << endl;
 
+        cout << "test operatore ()" << endl;
+        cout << "valore non modificato " << matrice1(1, 1, 1) << endl;
+        matrice1(1, 1, 1) = 5;
+        cout << "valore modificato " << matrice1(1, 1, 1) << endl;
 
+        cout << endl;
+
+        cout << "test getter size" << endl;
+        //matrice1.size_z() = 1;
+        //erroe in compilazione indica che non posso modificare la size con questi operatori
+        cout << "size z: " << matrice1.size_z() << endl;
+        cout << "size y: " << matrice1.size_y() << endl;
+        cout << "size x: " << matrice1.size_x() << endl;
+        cout << endl;
+
+        //vedremo meglio questi test nella serializzazione
+        cout << "test operatore <<" << endl;
+        cout << matrice1;
+        cout << endl;
+
+        cout << "test metodo slice()<<" << endl;
+        matrix3D<double> matrice_tagliata = matrice1.slice(100, 2, 1);
+        cout << matrice_tagliata;
+        cout << endl;
+    }
+}
+void test_matrice_costante_primitivi(bool set = true) 
+{
+    if (set == true) {
+        cout << "---------------------Test matrice costante di primitivi---------------------" << endl;
+        cout << "creazione matrice costante" << endl;
+        matrix3D<int> const matrice_costante(2, 3, 4, 10);
+        cout <<"prova stampa valore "<< matrice_costante(1, 1, 1) << endl;
+        //matrice_costante(1, 1, 1) = 2;
+        //errore in compilazione
+    }
+}
 int main()
 {
+    #ifndef NDEBUG
     cout << "Test classe templata matrice3D" << endl;
 
-    test_metodi_fonamentali();
-    
+    test_metodi_fonamentali_primitivi();
+    test_metodi_ausiliari_primitivi();
+    test_matrice_costante_primitivi();
+    #endif
 }
 
--- Matrix3D/Matrice3D/Matrice3D/matrix3D.h
+++ Matrix3D/Matrice3D/Matrice3D/matrix3D.h
@@ -33,7 +33,52 @@ public:
         cout << "matrice vuota creata" << endl;
     #endif
     }
+    matrix3D(size_type size_z, size_type size_y, size_type size_x) :
+        _matrix(nullptr), sz_z(0), sz_y(0), sz_x(0)
+    {
+        assert(size_z > 0);
+        assert(size_y > 0);
+        assert(size_x > 0);
+        try {
+            sz_z = size_z;
+            sz_y = size_y;
+            sz_x = size_x;
 
+            //creazione array dei piani
+            _matrix = new T * *[size_z];
+
+            for (size_type k = 0; k < size_z; ++k) {
+
+                //creazione degli array delle righe
+                _matrix[k] = new T * [size_y];
+
+                for (size_type j = 0; j < size_y; ++j) {
+
+                    //creazione degli array delle colonne
+                    _matrix[k][j] = new T[size_x];
+
+                    //le celle non sono inizializzate
+
+            
+
+
+                }
+
+            }
+            //stampa di debug
+            print_matrix("matrice creata");
+
+        }
+        //questo blocco try catch serve nel caso venga lanciata un eccezione dal fallimento di una new
+        //in questo caso la classe potrebbe essere in uno stato incoerente, e quindi effettuo una clear() 
+        //per riportarla ad uno stato coerente e rilancio l'eccezione
+        //questo blocco verrà ripetuto più volte nel progetto, questi commenti sono da considerarsi vaidi 
+        catch (...)
+        {
+            clear();//evito memory leack
+            throw;//rilancio l'eccezzione
+        }
+    }
     matrix3D(size_type size_z, size_type size_y, size_type size_x, const T &default_value) : 
         _matrix(nullptr), sz_z(0), sz_y(0), sz_x(0)
 	{
@@ -130,7 +175,7 @@ public:
         //questo blocco verrà ripetuto più volte nel progetto, questi commenti sono da considerarsi vaidi 
         catch (...)
         {
-            clear();//evito memory leack
+            clear();//evito memory leak
             throw;//rilancio l'eccezzione
         }
     }
@@ -145,7 +190,23 @@ public:
         return *this;
     }
 
-    T& operator()(size_type z, size_type y, size_type x) const {
+    size_type size_z(void) const{
+        return sz_z;
+    }
+
+    size_type size_y(void) const {
+        return sz_y;
+    }
+
+    size_type size_x(void) const {
+        return sz_x;
+    }
+
+    T& operator()(size_type z, size_type y, size_type x) {
+
+        return _matrix[z][y][x];
+    }
+    const T& operator()(size_type z, size_type y, size_type x) const {
         
         return _matrix[z][y][x];
     }
@@ -185,27 +246,27 @@ public:
 
     friend std::ostream& operator<<(std::ostream& os,
         const matrix3D<T>& mat) {
-        os << "{size z: " << mat.sz_z << std::endl;
-        os << "size y: " << mat.sz_y << std::endl;
-        os << "size x: " << mat.sz_x << "}" << std::endl;
-        os << "{";
+        os << mat.sz_z << ' ';
+        os << mat.sz_y << ' ';
+        os << mat.sz_x << ' ' << std::endl;
+        
         for (size_type k = 0; k < mat.sz_z; ++k) {
             if(k!=0)
             os << std::endl;
-            os << "[ ";
+            
             for (size_type j = 0; j < mat.sz_y; ++j) {
-                if(j!=0)
+              
                 os << endl;
 
                 for (size_type i = 0; i < mat.sz_x; ++i) {
-                    os << mat(k,j,i) << " ";
+                    os << mat(k,j,i) << ' ';
                 }
                 
             }
-            os << "]";
+         
 
         }
-        os << "}" <<std::endl;
+        os << std::endl;
         return os;
     }
     void swap(matrix3D& other) {
@@ -215,28 +276,74 @@ public:
         std::swap(sz_z, other.sz_z);
     }
 
+    matrix3D slice(const size_type size_z, const size_type size_y, const size_type size_x) const{
+        assert(size_z <= sz_z);
+        assert(size_y <= sz_y);
+        assert(size_x <= sz_x);
+        matrix3D<T> return_matrix(size_z, size_y, size_x);
+
+        for (size_type k = 0; k < size_z; ++k) {//scorro i piani
+
+            for (size_type j = 0; j < size_y; ++j) {//scorro le righe
+
+                for (size_type i = 0; i < size_x; ++i) {//scorro le colonne
+                    //imposto il nuovo valore in base a cosa è contenuto nella matrice di partenza
+                    return_matrix(k, j, i) = _matrix[k][j][i];
+
+                }
+
+            }
+
+        }
+
+        return return_matrix;
+    }
+
+
     private:
     
     void print_matrix(const string message = "", const bool enabled = true) const
     {
-       /* for (size_type k = 0; k < sz_z; ++k) {
-            cout << "pagina " << k<< ":" <<endl;
-            
-            for (size_type j = 0; j < sz_y; ++j) {
-                
-
-                for (size_type i = 0; i < sz_x; ++i) {
-                    cout << _matrix[k][j][i] << " ";
-                }
-                cout << endl;
-            }
-
-        }*/
+     
         #ifndef NDEBUG
         if (enabled == true) 
         {
             cout << message<<endl;
-            cout << *this;
+            /* for (size_type k = 0; k < sz_z; ++k) {
+          cout << "pagina " << k<< ":" <<endl;
+
+          for (size_type j = 0; j < sz_y; ++j) {
+
+
+              for (size_type i = 0; i < sz_x; ++i) {
+                  cout << _matrix[k][j][i] << " ";
+              }
+              cout << endl;
+          }
+
+      }*/
+        cout << "{size z: " << sz_z << std::endl;
+        cout << "size y: " << sz_y << std::endl;
+        cout << "size x: " << sz_x << "}" << std::endl;
+        cout << "{";
+        for (size_type k = 0; k < sz_z; ++k) {
+            if(k!=0)
+            cout << std::endl;
+            cout << "[ ";
+            for (size_type j = 0; j < sz_y; ++j) {
+                if(j!=0)
+                cout << endl;
+
+                for (size_type i = 0; i < sz_x; ++i) {
+                    cout << _matrix[k][j][i] << " ";
+                }
+                
+            }
+            cout << "]";
+
+        }
+        cout << "}" <<std::endl;
+        
         }
         #endif
         
